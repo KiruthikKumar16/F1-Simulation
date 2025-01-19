9428474adbc625064ed280f9b1e254b99c974700
kiruthik kumar M <m.kiruthikkumar@gmail.com>
2025-01-19 22:13:55 +0530

    Update DateAscComparator.java

--- FORMULA-1_CHAMPIONSHIP_MANAGER-main/F1_Manager/src/com/company/DateAscComparator.java
+++ FORMULA-1_CHAMPIONSHIP_MANAGER-main/F1_Manager/src/com/company/DateAscComparator.java
@@ -2,16 +2,16 @@ package com.company;
 
 import java.util.Comparator;
 
-public class DateAscComparator implements Comparator <Race>{// comparator for sort the dates in ascending order
+public class DateAscComparator implements Comparator <Race>{
 
     @Override
-    public int compare(Race d1, Race d2) { // date comparator
-        if (d1.getDate().compareTo(d2.getDate())==0){ // if d1 equals d2
+    public int compare(Race d1, Race d2) {
+        if (d1.getDate().compareTo(d2.getDate())==0){ 
             return 0;
-        }else if (d1.getDate().compareTo(d2.getDate())>0) { // if d1 > d2 its a positive value so  should return 1
+        }else if (d1.getDate().compareTo(d2.getDate())>0) { 
             return 1;
         }else{
-            return -1; //if d1 < d2 its a negative value so  should return -1
+            return -1; 
         }
     }
 }
